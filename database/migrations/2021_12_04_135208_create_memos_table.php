6f2c96823f261db12a206dd415132f4829600cff
y-soliloquy <engineer.soliloquy@gmail.com>
2021-12-31 19:35:58 +0900

    memosテーブルに処理を追加

--- database/migrations/2021_12_04_135208_create_memos_table.php
+++ database/migrations/2021_12_04_135208_create_memos_table.php
@@ -14,8 +14,14 @@ class CreateMemosTable extends Migration
     public function up()
     {
         Schema::create('memos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id', true);
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            // 論理削除を定義（deleted_atを自動生成する）
+            $table->softDeletes();
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
