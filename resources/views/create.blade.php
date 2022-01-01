09be3e36e9c71f6e1a248dcd348eb840f806d975
y-soliloquy <engineer.soliloquy@gmail.com>
2022-01-01 14:01:23 +0900

    csrfトークンを発行できるようにしクロスサイトリクエストフォージェリ対策をする

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,6 +4,7 @@
 <div class="card">
     <div class="card-header">新規メモ作成</div>
     <form class="card-body" action="/store" method="POST">
+        @scrf
         <div class="form-group">
             <textarea class="form-control" name="content" rows="3" placeholder="メモを入力"></textarea>
         </div>
