a390c72230fd1771e75cf99916ea704d4ad9c449
y-soliloquy <engineer.soliloquy@gmail.com>
2022-01-01 19:05:20 +0900

    routeメソッドを利用してformのactionのルーティングを変数管理に変更する

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="card">
     <div class="card-header">新規メモ作成</div>
-    <form class="card-body" action="/store" method="POST">
+    <form class="card-body" action="{{ route('store') }}" method="POST">
         @scrf
         <div class="form-group">
             <textarea class="form-control" name="content" rows="3" placeholder="メモを入力"></textarea>
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::post('/store', [HomeController::class, 'store'])->name('home');
+Route::post('/store', [HomeController::class, 'store'])->name('store');
