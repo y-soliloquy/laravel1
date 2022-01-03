c2d398038eac93013a35b02138762b986d8a0b44
y-soliloquy <engineer.soliloquy@gmail.com>
2022-01-04 06:48:01 +0900

    メモ編集用のviewを用意しidに紐づいたメモ内容をデフォルトで表示させる

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-header">メモ編集</div>
+    <form class="card-body" action="{{ route('store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <textarea class="form-control" name="content" rows="3" placeholder="メモを入力">{{ $edit_memo['content'] }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">更新</button>
+    </form>
+</div>
+@endsection
